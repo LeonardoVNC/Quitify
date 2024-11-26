80fda04787a1e50e2117db731be78ee4a033112d
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2024-11-26 08:50:47 -0400

    Actualizando target.Sdk de 34 a 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.noodlenetworkplus"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.noodlenetworkplus"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
