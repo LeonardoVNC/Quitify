10754ea8ed631fda147223f37bb5092d87e2aadb
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2024-12-10 17:07:06 -0400

    Se implementa una primera versión de DetallesCuentaActivity, aun se requiere acceder a archivos del registro para completar la lógica. Se implementa una confirmación de seguridad para el cierre de sesión, aun es necesario hacer efectivo el cierre.

--- app/src/main/java/com/example/noodlenetworkplus/ConfiguracionActivity.kt
+++ app/src/main/java/com/example/noodlenetworkplus/ConfiguracionActivity.kt
@@ -3,6 +3,7 @@ package com.example.noodlenetworkplus
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,14 +20,30 @@ class ConfiguracionActivity : BaseActivity() {
 
         binding.configButtonBack.setOnClickListener{onBackPressed()}
         binding.configButtonAccount.setOnClickListener{
-            //TODO Implementar esta pantalla
+            val intent = Intent(this, DetallesCuentaActivity::class.java)
+            startActivity(intent)
         }
         binding.configButtonTheme.setOnClickListener{
             val intent = Intent(this, SelectThemeActivity::class.java)
             startActivity(intent)
         }
         binding.configButtonCloseAccount.setOnClickListener{
-            //TODO Implementar esta pantalla
+                showConfirmarCierreSesion()
         }
+    }
+
+    private fun showConfirmarCierreSesion() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.closeAccountTitle))
+        builder.setMessage(getString(R.string.closeAccountContent))
+
+        builder.setPositiveButton(getString(R.string.closeAccountPositive)) { dialog, which ->
+            //TODO Implementar esta función
+        }
+        builder.setNegativeButton(getString(R.string.closeAccountNegative)) { dialog, which ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
