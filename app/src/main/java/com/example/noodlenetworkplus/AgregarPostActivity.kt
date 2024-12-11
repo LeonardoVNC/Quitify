2267f9e81e6187d1c446ea2292b22335b8bdfc94
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2024-12-11 18:24:02 -0400

    Implementación correcta de la subida y descarga de Publicaciones usando sharedPreferences y JSON como apoyo.

--- app/src/main/java/com/example/noodlenetworkplus/AgregarPostActivity.kt
+++ app/src/main/java/com/example/noodlenetworkplus/AgregarPostActivity.kt
@@ -1,14 +1,18 @@
 package com.example.noodlenetworkplus
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.noodlenetworkplus.ForoDeComunidadActivity.Companion.ID_LIST_PUBLICACIONES
+import com.example.noodlenetworkplus.ForoDeComunidadActivity.Companion.ID_PUBLICACIONES
+import com.example.noodlenetworkplus.ForoDeComunidadActivity.Companion.gson
+import com.example.noodlenetworkplus.dataClasses.Publicacion
 import com.example.noodlenetworkplus.databinding.ActivityAgregarPostBinding
+import com.google.gson.reflect.TypeToken
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class AgregarPostActivity : AppCompatActivity() {
+class AgregarPostActivity : BaseActivity() {
     private lateinit var binding: ActivityAgregarPostBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +22,38 @@ class AgregarPostActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.postButtonAdd.setOnClickListener{
-            //TODO se debe guardar el editText.text en los JSON
-            println("Se deberia guardar el siguiente texto: ${binding.postEditText.text}")
+            //TODO Aun es necesario agregar el nombre de usuario y establecer funcionalidad de la imágen
+            agregarPublicacion("test", binding.postEditText.text.toString(), R.drawable.add)
 
             binding.postEditText.text.clear()
             val intent = Intent(this, ForoDeComunidadActivity::class.java)
             startActivity(intent)
         }
         binding.postButtonBack.setOnClickListener{onBackPressed()}
+    }
+
+    fun agregarPublicacion(usuario: String, contenido: String, idImagen: Int) {
+        val post = mutableListOf(
+            Publicacion (
+            autor = usuario,
+            fechaDePublicacion = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yy")).toString(),
+            contenido = contenido,
+            imagen = idImagen
+            )
+        )
+        post.addAll(cargarPublicaciones())
+        guardarPublicaciones(post)
+    }
+
+    fun guardarPublicaciones(publicaciones: MutableList<Publicacion>) {
+        val editor = this.getSharedPreferences(ID_LIST_PUBLICACIONES, Context.MODE_PRIVATE).edit()
+        editor.putString(ID_PUBLICACIONES, gson.toJson(publicaciones))
+        editor.apply()
+    }
+
+    fun cargarPublicaciones(): MutableList<Publicacion> {
+        val json = this.getSharedPreferences(ID_LIST_PUBLICACIONES, Context.MODE_PRIVATE).getString(ID_PUBLICACIONES, null)
+        val tipo = object : TypeToken<List<Publicacion>>() {}.type
+        return if (json != null) gson.fromJson(json, tipo) else mutableListOf<Publicacion>()
     }
 }
--- app/src/main/java/com/example/noodlenetworkplus/ForoDeComunidadActivity.kt
+++ app/src/main/java/com/example/noodlenetworkplus/ForoDeComunidadActivity.kt
@@ -1,37 +1,36 @@
 package com.example.noodlenetworkplus
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noodlenetworkplus.adapters.ForoDeComunidadAdapter.ForoDeComunidadAdapter
 import com.example.noodlenetworkplus.dataClasses.Publicacion
 import com.example.noodlenetworkplus.databinding.ActivityForoDeComunidadBinding
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class ForoDeComunidadActivity : BaseActivity() {
     private lateinit var binding: ActivityForoDeComunidadBinding
     private val recyclerPostAdapter by lazy {ForoDeComunidadAdapter()}
-    private var listaPublicaciones = mutableListOf<Publicacion>()
+
+    companion object{
+        val gson = Gson()
+        val ID_LIST_PUBLICACIONES = "LISTA_PUBLICACIONES"
+        val ID_PUBLICACIONES = "PUBLICACIONES"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForoDeComunidadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        //TEST
-        agregarPublicacion("User1", "publicando algo muy seriooo", R.drawable.add)
-        agregarPublicacion("User2", "publicando algo menos seriooo", R.drawable.menu3)
-        agregarPublicacion("User3", "publicando algo nada seriooo", R.drawable.community)
-        agregarPublicacion("User4", "publicando algo demasiado seriooo", R.drawable.arrow_back)
-        //TEST
         setUpRecyclerView()
 
         binding.communityButtonAdd.setOnClickListener {
             val intent = Intent(this, AgregarPostActivity::class.java)
             startActivity(intent)
         }
-
         binding.communityButtonTime.setOnClickListener {
             val intent = Intent(this, PrincipalActivity::class.java)
             startActivity(intent)
@@ -39,21 +38,16 @@ class ForoDeComunidadActivity : BaseActivity() {
     }
 
     fun setUpRecyclerView() {
-        recyclerPostAdapter.addDataToList(listaPublicaciones)
+        recyclerPostAdapter.addDataToList(cargarPublicaciones())
         binding.communityRecyclerPosts.apply() {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = recyclerPostAdapter
         }
     }
 
-    fun agregarPublicacion(usuario: String, contenido: String, idImagen: Int) {
-        val post = mutableListOf(Publicacion (
-            autor = usuario,
-            fechaDePublicacion = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yy")).toString(),
-            contenido = contenido, imagen = idImagen)
-        )
-        post.addAll(listaPublicaciones)
-        listaPublicaciones = post
-        setUpRecyclerView()
+    fun cargarPublicaciones(): MutableList<Publicacion> {
+        val json = this.getSharedPreferences(ID_LIST_PUBLICACIONES, Context.MODE_PRIVATE).getString(ID_PUBLICACIONES, null)
+        val tipo = object : TypeToken<List<Publicacion>>() {}.type
+        return if (json != null) gson.fromJson(json, tipo) else mutableListOf<Publicacion>()
     }
 }
