38d5516dda12c5ede6660824fc2ea69b42efe746
28charly7 <gabriel.charly.tauro@gmail.com>
2024-12-13 09:36:09 -0400

    Implementacion de RecyclerView para las Actividades (Aun me faltan las separación por categorias)

--- app/src/main/java/com/example/noodlenetworkplus/ActividadesActivity.kt
+++ app/src/main/java/com/example/noodlenetworkplus/ActividadesActivity.kt
@@ -6,17 +6,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.noodlenetworkplus.adapter.ActividadesAdapter.ActividadesAdapter
+import com.example.noodlenetworkplus.dataClasses.Actividad
 import com.example.noodlenetworkplus.databinding.ActivityActividadesBinding
 
 class ActividadesActivity : BaseActivity() {
     private lateinit var binding: ActivityActividadesBinding
+    private val recyclerActivityAdapter by lazy { ActividadesAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityActividadesBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        // TODO setUpRecyclerView
+        setUpRecyclerView()
         binding.recomendationButtonMenu.setOnClickListener {
             val intent = Intent(this, ConfiguracionActivity::class.java)
             startActivity(intent)
@@ -28,6 +32,18 @@ class ActividadesActivity : BaseActivity() {
         binding.recomendationButtonCommunity.setOnClickListener(){
             val intent = Intent(this,ForoDeComunidadActivity::class.java)
             startActivity(intent)
+        }
+    }
+    fun setUpRecyclerView(){
+        val listaDatos = mutableListOf(
+            Actividad(titulo="Dominadas",descripcion="Has 50 dominadas con el balon"),
+            Actividad(titulo="Canastas",descripcion="Has 50 dominadas con el balon"),
+            Actividad(titulo="Tenis de mesa",descripcion="Has 50 dominadas con el balon")
+        )
+        recyclerActivityAdapter.addDataToList(listaDatos)
+        binding.communityRecyclerPosts.apply() {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = recyclerActivityAdapter
         }
     }
 }
