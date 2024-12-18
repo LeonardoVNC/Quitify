fdedd378f685d1a3c2f348ace55fea5f28f510b2
28charly7 <gabriel.charly.tauro@gmail.com>
2024-12-18 13:27:44 -0400

    Documentacion del codigo ya añadido posteriormente

--- app/src/main/java/com/example/noodlenetworkplus/ActividadesActivity.kt
+++ app/src/main/java/com/example/noodlenetworkplus/ActividadesActivity.kt
@@ -14,42 +14,44 @@ import com.example.noodlenetworkplus.dataClasses.Actividad
 import com.example.noodlenetworkplus.databinding.ActivityActividadesBinding
 
 class ActividadesActivity : BaseActivity() {
+    //Objeto estatido para para definir constantes metodos accesibles sin necesidad de instanciar la clase
     companion object {
         val ID_CATEGORIA = "CATEGORIA"
     }
 
-    private lateinit var binding: ActivityActividadesBinding
+    private lateinit var binding: ActivityActividadesBinding//Inicialización tardia de binding para la interacción con las vistas
     private val recyclerActivityAdapter by lazy { ActividadesAdapter() }
-    private val listaDeDatos = mutableListOf<Actividad>()
+    private val listaDeDatos = mutableListOf<Actividad>()//lista mutable que guarda la actividad
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tipo = intent.getStringExtra(ID_CATEGORIA) ?: "error"
+        val tipo = intent.getStringExtra(ID_CATEGORIA) ?: "error"//Recuperar el dato extra "Categoria" con Intent, si este es vacio entonces devolvera error
         binding = ActivityActividadesBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        verificarCategoria(tipo)
-        setUpRecyclerView()
+        verificarCategoria(tipo)//Verificacion de los dato segun la categoria
+        setUpRecyclerView()//configuracion del RecyclerView segun los datos y el adapter
 
-        binding.recomendationButtonMenu.setOnClickListener {
-            val intent = Intent(this, ConfiguracionActivity::class.java)
-            startActivity(intent)
+        binding.recomendationButtonMenu.setOnClickListener { // Reconoce si el boton "ButtonMenu" fue presionado
+            val intent = Intent(this, ConfiguracionActivity::class.java) // lleva a la pantalla "Configuracion"
+            startActivity(intent) //comienza el intent
         }
-        binding.recomendationButtonTime.setOnClickListener(){
-            val intent = Intent(this,PrincipalActivity::class.java)
-            startActivity(intent)
+        binding.recomendationButtonTime.setOnClickListener(){ // Reconoce si el boton "ButtonTime" fue presionado
+            val intent = Intent(this,PrincipalActivity::class.java) // lleva a la pantalla "Principal"
+            startActivity(intent)//comienza el intent
         }
-        binding.recomendationButtonCommunity.setOnClickListener(){
-            val intent = Intent(this,ForoDeComunidadActivity::class.java)
-            startActivity(intent)
+        binding.recomendationButtonCommunity.setOnClickListener(){ // Reconoce si el boton "ButtonCommunity" fue presionado
+            val intent = Intent(this,ForoDeComunidadActivity::class.java) // lleva a la pantalla "Foro de Comunidad"
+            startActivity(intent)//comienza el intent
         }
     }
-    fun verificarCategoria(categoria: String){
-        var list = mutableListOf<Actividad>()
-        when(categoria){
-            "Deporte" -> {
-                 list = mutableListOf(
+    fun verificarCategoria(categoria: String){//Funcion que recibe un String
+        var list = mutableListOf<Actividad>()//Creaccion de una lista mutable pra alamacenar objetos de tipo actividad
+        when(categoria){//Reconoce que tipo de Actividad es segun la categoria
+            "Deporte" -> {// Si el boton presionado tiene el nombre de "Deporte" entonces:
+                 list = mutableListOf(//Se llena la lista con los siguiente datos
+                     //Actividades
                      Actividad(titulo = "Dominadas", descripcion = "Intenta realizar 10 dominadas seguidas sin fallar. Mantén la concentración y demuestra tu fuerza. ¡Tú puedes lograrlo!","",""),
                      Actividad(titulo = "Canastas", descripcion = "Desafíate a encestar 5 tiros de 3 puntos seguidos. Controla tu puntería y demuestra tu habilidad. ¿Listo para intentarlo?","",""),
                      Actividad(titulo = "Digita", descripcion = "Realiza 20 digitaciones manteniendo el balón en el aire sin que caiga al suelo. ¡Mantén el control y no te rindas!","",""),
@@ -59,8 +61,9 @@ class ActividadesActivity : BaseActivity() {
                      Actividad(titulo = "Tiros con efecto", descripcion = "Practica disparos con efecto hacia una portería o blanco específico. ¡Ponle curva al balón y desafía tu precisión!","","")
                  )
             }
-            "Dibujo" -> {
-                list = mutableListOf(
+            "Dibujo" -> {//  Si el boton presionado tiene el nombre de "Dibujo" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Boceto rápido", descripcion = "Crea un boceto en solo 10 minutos. No importa la perfección, deja que fluya tu creatividad","",""),
                     Actividad(titulo = "Paisaje imaginario", descripcion = "Dibuja un paisaje de tu imaginación, con montañas, ríos o lo que te inspire. ¡Dale vida a tus ideas!","",""),
                     Actividad(titulo = "Personaje único", descripcion = "Diseña un personaje original con detalles únicos. ¡Deja que tu estilo brille en cada trazo!", "",""),
@@ -72,8 +75,9 @@ class ActividadesActivity : BaseActivity() {
                     Actividad(titulo = "Escena de tu día", descripcion = "Dibuja una escena de algo que hiciste hoy, como tu habitación, una comida o una caminata.","","")
                 )
             }
-            "Cocina" -> {
-                list = mutableListOf(
+            "Cocina" -> {//  Si el boton presionado tiene el nombre de "Cocina" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Prepara un budín", descripcion = "Sigue una receta sencilla y prepara un budín suave y delicioso. Una pequeña ayuda para prepararlo: ", "Budin delicioso","https://www.youtube.com/watch?v=iTsmADW4f2M"),
                     Actividad(titulo = "Haz una pizza casera", descripcion = "Crea una pizza desde cero. Amasa la base, agrega salsa de tomate, queso y tus toppings favoritos, ¡y hornéala hasta que quede perfecta!", "Pizza Casera", "https://www.youtube.com/watch?v=crdtrzZj3fo&pp=ygUbcGl6emEgY2FzZXJhIGZhY2lsIHkgcmFwaWRh"),
                     Actividad(titulo = "Batido refrescante", descripcion = "Prepara un batido con frutas frescas, leche o yogurt, y un toque de miel. ¡Ideal para un día caluroso!","Batidos para un dia caluroso", "https://www.youtube.com/watch?v=qPDwGpmJSuE&pp=ygUSYmF0aWRvIHJlZnJlc2NhbnRl"),
@@ -84,8 +88,9 @@ class ActividadesActivity : BaseActivity() {
                     Actividad(titulo = "Panqueques caseros", descripcion = "Haz panqueques esponjosos y decóralos con frutas, miel o chocolate. ¡Perfectos para un desayuno especial!","Panqueques Esponjosos","https://www.youtube.com/watch?v=JUeCNvCAnL8&pp=ygUScGFucXVlcXVlcyBjYXNlcm9z")
                 )
             }
-            "Musica" -> {
-                list = mutableListOf(
+            "Musica" -> {//  Si el boton presionado tiene el nombre de "Musica" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Escribe una canción", descripcion = "Usa un cuaderno y escribe la letra de una canción. Agrega un ritmo básico golpeando la mesa o usando una aplicación musical.","",""),
                     Actividad(titulo = "Crea un playlist único", descripcion = "Selecciona tus canciones favoritas y crea un playlist que refleje tu estado de ánimo. ¡Perfecto para inspirarte o relajarte!","",""),
                     Actividad(titulo = "Dibuja tu música", descripcion = "Escucha una canción y dibuja lo que sientes o lo que te inspira. Transforma los sonidos en imágenes.","",""),
@@ -96,8 +101,9 @@ class ActividadesActivity : BaseActivity() {
                     Actividad(titulo = "Sonidos con flauta", descripcion = "Si tienes una flauta dulce, aprende a tocar notas básicas y una melodía simple. ¡Es ideal para empezar!","","")
                 )
             }
-            "Poesia" -> {
-                list = mutableListOf(
+            "Poesia" -> {//  Si el boton presionado tiene el nombre de "Poesia" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Escribe un haiku", descripcion = "Crea un poema breve siguiendo la estructura del haiku: tres versos de 5, 7 y 5 sílabas. Inspírate en la naturaleza o en tus pensamientos.","",""),
                     Actividad(titulo = "Crea un acróstico", descripcion = "Elige una palabra especial y escribe un poema donde cada verso comience con una letra de esa palabra.","",""),
                     Actividad(titulo = "Describe tu día en versos", descripcion = "Escribe un poema corto que narre cómo ha sido tu día. Usa rimas o versos libres para expresar tus emociones.","",""),
@@ -111,19 +117,21 @@ class ActividadesActivity : BaseActivity() {
                     Actividad(titulo = "Improvisa versos con rimas", descripcion = "Escribe versos rápidos usando palabras que rimen entre sí. ¡No te preocupes por la perfección, diviértete creando!","","")
                 )
             }
-            "Aprendizaje" -> {
-                list = mutableListOf(
+            "Aprendizaje" -> {//  Si el boton presionado tiene el nombre de "Aprendizaje" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Aprende 5 palabras nuevas en otro idioma", descripcion = "Elige un idioma que te interese y busca 5 palabras nuevas. Escríbelas, di su pronunciación y úsalas en oraciones simples.","",""),
                     Actividad(titulo = "Domina una fórmula matemática", descripcion = "Escoge una fórmula o concepto matemático. Aprende cómo se usa y resuelve 3 problemas aplicándola paso a paso.","",""),
                     Actividad(titulo = "Crea un mapa mental", descripcion = "Elige un tema que estés estudiando y haz un mapa mental que conecte ideas clave, conceptos y ejemplos. Usa colores y dibujos que te ayuden a recordarlo mejor.","",""),
                     Actividad(titulo = "Estudia con flashcards", descripcion = "Crea tarjetas de memoria (flashcards) con conceptos importantes. Son un buen metodo para aprender cosas nuevas, es muy usada para el estudio.","",""),
-                    Actividad(titulo = "Investiga un dato curioso historico", descripcion = "Elige un evento histórico o personaje famoso, talvez ya tienes un favortio, y descubre un hecho interesante sobre él. Anótalo y compártelo con alguien.","Dato curioso","https://blog.3bee.com/es/tassodelmiele-3bee-es/"),
+                    Actividad(titulo = "Investiga un dato curioso historico", descripcion = "Elige un evento histórico o personaje famoso, talvez ya tienes un favortio, y descubre un hecho interesante sobre él. Anótalo y compártelo con alguien.","Dato curioso","https://www.autopista.es/planeta2030/doce-curiosidades-historia_255516_102.html"),
                     Actividad(titulo = "Aprende una técnica de estudio nueva", descripcion = "Investiga técnicas como el Pomodoro, mapas conceptuales o resúmenes. Practica la técnica durante 25 minutos sobre un tema que te guste.","",""),
                     Actividad(titulo = "Identifica las partes de un sistema (cuerpo, máquina, etc.)", descripcion = "Elige un sistema específico: Humano o Maquina. Dibuja sus partes y nómbralas mientras describes su función.","",""),
                 )
             }
-            "Videojuegos" -> {
-                list = mutableListOf(
+            "Videojuegos" -> {//  Si el boton presionado tiene el nombre de "Videojuegos" entonces:
+                list = mutableListOf(//Se llena la lista con los siguiente datos
+                    //Actividades
                     Actividad(titulo = "Organiza un torneo con amigos", descripcion = "Elige tu videojuego favorito y organiza un torneo con amigos. Define las reglas, crea brackets y compite para ver quién es el campeón.","",""),
                     Actividad(titulo = "Completa un desafío en tu juego favorito", descripcion = "Selecciona un desafío difícil dentro de tu juego preferido, como completar todas las misiones secundarias","",""),
                     Actividad(titulo = "Crea tu propio mundo o nivel", descripcion = "Utiliza herramientas de creación en juegos como Minecraft, Roblox o Super Mario Maker para diseñar un mundo, nivel o escenario a tu gusto.","",""),
@@ -133,17 +141,17 @@ class ActividadesActivity : BaseActivity() {
                     Actividad(titulo = "Desbloquea logros o trofeos", descripcion = "Revisa los logros o trofeos de un juego y trata de completar la mayor cantidad posible. Algunos desafíos requieren mucha habilidad.","","")
                 )
             }
-            else -> {
-                println("Noodle")
+            else -> {// Si no es ninguno de los anteriores entonces:
+                println("Noodle") //imprime en la consola
             }
         }
-        listaDeDatos.addAll(list)
+        listaDeDatos.addAll(list)// Añadir todos los elementos de la lista "list" a la lista de datos mustable "listaDeDatos"
     }
-    fun setUpRecyclerView(){
+    fun setUpRecyclerView(){//Añade al adaptador la "listaDeDatos"
         recyclerActivityAdapter.addDataToList(listaDeDatos)
         binding.communityRecyclerPosts.apply() {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            adapter = recyclerActivityAdapter
+            adapter = recyclerActivityAdapter//Asignar el adaptador al RecyclerView
         }
     }
 }
